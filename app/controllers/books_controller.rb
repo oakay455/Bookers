e333bf4305497d4b7422c7f5b63bb2a573332158
oakay455 <ayako110.89@gmail.com>
2022-04-10 11:46:15 +0000

    [Add]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,21 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+　　@book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+     render :index
+    end
   end
 
   def index
     @books = Book.all
+    @books = Book.all.order(created_at: :desc)
+    @book = Book.new
   end
 
   def show
@@ -18,11 +23,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book =Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   def destroy
     @book = Book.find(params[:id])
-    book.destroy
+    @book.destroy
     redirect_to books_path
   end
 
